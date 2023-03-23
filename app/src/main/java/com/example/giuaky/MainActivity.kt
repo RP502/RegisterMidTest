6d25c0dc34c53cfbd89d55baa4243929b1dfd590
Phuong Dinh Viet (RP005) <101784156+RP502@users.noreply.github.com>
2023-03-23 09:19:04 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/giuaky/MainActivity.kt
+++ app/src/main/java/com/example/giuaky/MainActivity.kt
@@ -8,13 +8,20 @@ import kotlinx.android.synthetic.main.activity_singnup.*
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var btnDangKy: Button
+     private  lateinit var btnDangNhap: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnDangKy = findViewById(R.id.btnDangky)
+        btnDangNhap =  findViewById(R.id.btnDangNhap)
         btnDangKy.setOnClickListener {
             val intent = Intent(this, singnup::class.java)
+            startActivity(intent)
+        }
+        
+                btnDangNhap.setOnClickListener {
+            val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
     }
